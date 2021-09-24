d5973e82b4808bb14e459c8bf074472e3211e94a
Bill Tiffany <daganlegalservice@gmail.com>
2021-09-24 16:49:59 -0600

    updating password variables

--- password.js
+++ password.js
@@ -1,3 +1,5 @@
+
+
 console.log("Welcome to the password Verifier Tool")
 console.log("Please create a password that is at least 10 characters long")
 
@@ -9,6 +11,34 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
+const pcheck =reader.output
+function validatePassword(p) {
+    const errors = [];
+    if (p.length < 8) {
+        errors.push("Your password must be at least 8 characters");
+    }
+    if (p.length > 32) {
+        errors.push("Your password must be at max 32 characters");
+    }
+    if (p.search(/[a-z]/) < 0) {
+        errors.push("Your password must contain at least one lower case letter."); 
+    }
+    if (p.search(/[A-Z]/) < 0) {
+        errors.push("Your password must contain at least one upper case letter."); 
+    }
+
+    if (p.search(/[0-9]/) < 0) {
+        errors.push("Your password must contain at least one digit.");
+    }
+   if (p.search(/[!@#\$%\^&\*_]/) < 0) {
+        errors.push("Your password must contain at least special char from -[ ! @ # $ % ^ & * _ ]"); 
+    }
+    if (errors.length > 0) {
+        console.log(errors.join("\n"));
+        return false;
+    }
+    return true;
+}
 reader.question(`Please Enter a Password you'd like to verify:`, password => {
     String(password);
     if(password.length >= 10){
@@ -27,3 +57,5 @@ reader.question(`Please Enter a Password you'd like to verify:`, password => {
                 reader.close();
     }}
 )}})
+
+
